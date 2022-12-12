05ead5efd4d3057ff61dd1185faf57445c3f6775
Pedro Henrique de Jesus Sousa <pedrohjs11@gmail.com>
2022-12-12 11:22:46 -0300

    VeiculoDao
    implementados metodos na classe de VeiculoDao

--- ProjetoLocaGyn28_10/src/com/locagyn/persistencia/LocacaoDao.java
+++ ProjetoLocaGyn28_10/src/com/locagyn/persistencia/LocacaoDao.java
@@ -4,10 +4,15 @@
  */
 package com.locagyn.persistencia;
 
+import com.locagyn.enums.EnumSituacao;
 import com.locagyn.ferramentas.GeradorIdentificador;
 import com.locagyn.modelos.Locacao;
+import com.locagyn.modelos.Veiculo;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,27 +20,89 @@ import java.io.FileWriter;
  */
 public class LocacaoDao implements ILocacaoDao{
     private String nomeDoArquivo = ".\\src\\com\\locagyn\\arquivosdedados\\Locacao.txt";
+    
     @Override
     public void locar(Locacao objeto) throws Exception {
-        try{    
+        try{
             FileWriter fw = new FileWriter(nomeDoArquivo,true);
             BufferedWriter bw =new BufferedWriter(fw);
             objeto.setId(GeradorIdentificador.getID());
             bw.write(objeto.toString()+"\n");
-            bw.close();		
+            bw.close();
+            
+            ArrayList<Locacao> listaDeLocacao = new ArrayList<Locacao>();
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br  = new BufferedReader(fr);
+            String linha = "";
+            while((linha=br.readLine())!=null){
+            Locacao objetoLocacao = new Locacao();
+            Veiculo objVeiculo = new Veiculo();
+            String vetorString[] = linha.split(";");
+            objetoLocacao.setId(Integer.parseInt(vetorString[0]));
+            //objetoLocacao.setObjVeiculo(veiculoDao.buscar(Integer.parseInt(vetorString[1])));
+            //objetoLocacao.setObjCliente(clienteDao.buscar(Integer.parseInt(vetorString[2])));
+            objVeiculo.setSituacao(EnumSituacao.LOCADO);
+            }
       }catch(Exception erro){
         throw erro;
+        
       }
     }
 
     @Override
     public void devolver(Locacao objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            FileWriter fw = new FileWriter(nomeDoArquivo,true);
+            BufferedWriter bw =new BufferedWriter(fw);
+            objeto.setId(GeradorIdentificador.getID());
+            bw.write(objeto.toString()+"\n");
+            bw.close();
+            
+            ArrayList<Locacao> listaDeLocacao = new ArrayList<Locacao>();
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br  = new BufferedReader(fr);
+            String linha = "";
+            while((linha=br.readLine())!=null){
+            Locacao objetoLocacao = new Locacao();
+            Veiculo objVeiculo = new Veiculo();
+            String vetorString[] = linha.split(";");
+            objetoLocacao.setId(Integer.parseInt(vetorString[0]));
+            //objetoLocacao.setObjVeiculo(veiculoDao.buscar(Integer.parseInt(vetorString[1])));
+            //objetoLocacao.setObjCliente(clienteDao.buscar(Integer.parseInt(vetorString[2])));
+            objVeiculo.setSituacao(EnumSituacao.DISPONIVEL);
+            }
+      }catch(Exception erro){
+        throw erro;
+        
+      }
     }
 
     @Override
     public void cancelar(Locacao objeto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            FileWriter fw = new FileWriter(nomeDoArquivo,true);
+            BufferedWriter bw =new BufferedWriter(fw);
+            objeto.setId(GeradorIdentificador.getID());
+            bw.write(objeto.toString()+"\n");
+            bw.close();
+            
+            ArrayList<Locacao> listaDeLocacao = new ArrayList<Locacao>();
+            FileReader fr = new FileReader(nomeDoArquivo);
+            BufferedReader br  = new BufferedReader(fr);
+            String linha = "";
+            while((linha=br.readLine())!=null){
+            Locacao objetoLocacao = new Locacao();
+            Veiculo objVeiculo = new Veiculo();
+            String vetorString[] = linha.split(";");
+            objetoLocacao.setId(Integer.parseInt(vetorString[0]));
+            //objetoLocacao.setObjVeiculo(veiculoDao.buscar(Integer.parseInt(vetorString[1])));
+            //objetoLocacao.setObjCliente(clienteDao.buscar(Integer.parseInt(vetorString[2])));
+            objVeiculo.setSituacao(EnumSituacao.DISPONIVEL);
+            }
+      }catch(Exception erro){
+        throw erro;
+        
+      }
     }
     
 }
