5292a78db751f1813a4fff94f0f68cedc7bd29d1
PedroHenriquedeJesus <pedrohjs11@gmail.com>
2022-12-13 22:16:39 -0300

    nada
    esqueci de salvar tela locacao

--- ProjetoLocaGyn28_10/src/com/locagyn/visao/TelaLocacao.java
+++ ProjetoLocaGyn28_10/src/com/locagyn/visao/TelaLocacao.java
@@ -156,6 +156,11 @@ public class TelaLocacao extends javax.swing.JFrame {
         jScrollPane3.setViewportView(jTable1);
 
         jButton1.setText("LOCAR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("DEVOLVER");
 
@@ -255,6 +260,14 @@ public class TelaLocacao extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        try {
+            
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
